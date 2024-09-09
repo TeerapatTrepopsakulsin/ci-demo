69245d2011bc86197f87aa1c32781464648c784d
TeerapatTREPOPSAKULSIN <teerapat.tre@ku.th>
2024-09-09 14:31:14 +0700

    Modify docstring

--- statistics.py
+++ statistics.py
@@ -1,3 +1,4 @@
+"""Basic Statistical Operation."""
 from math import sqrt
 
 
@@ -9,7 +10,7 @@ def average(data):
 
 
 def variance(data):
-    """the population variance of a list of numbers in data.
+    """Return the population variance of a list of numbers in data.
 
     The variance is the sum of squared differences between data values
     and their mean, divided by the number of items in the list.
@@ -32,7 +33,6 @@ def variance(data):
     >>> variance([1000000, 1000004])
     4.0
     """
-    # ugly code.
     n = len(data)
     if n == 0:
         raise ValueError("List must contain at least one value")
@@ -41,5 +41,5 @@ def variance(data):
 
 
 def stdev(data):
-    """the standard deviation of a list of values"""
+    """Return the standard deviation of a list of values."""
     return sqrt(variance(data))
